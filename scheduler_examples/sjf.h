6e3cfd81027c3a654c6a0f612603524cb2c60cce
GuilhermeFrantz22204098 <a22204098@alunos.ulht.pt>
2025-10-13 14:07:22 +0100

    Created sjf .c and .h files and adapted FIFO algorithm to SJF

---
+++ scheduler_examples/sjf.h
@@ -0,0 +1,12 @@
+//
+// Created by guilh on 12/10/2025.
+//
+
+#ifndef SJF_H
+#define SJF_H
+
+#include "queue.h"
+
+void sjf_scheduler(uint32_t current_time_ms, queue_t *rq, pcb_t **cpu_task);
+
+#endif //SJF_H
